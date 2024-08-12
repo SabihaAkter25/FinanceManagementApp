089ab968fb0e24c303acd45ebee887274e64422c
Sabiha <ssabiha104@gmail.com>
2024-08-12 16:37:04 -0700

    final commit

---
+++ lib/view/main_tabview/main_tab_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainTabView extends StatefulWidget {
+  const MainTabView({super.key});
+
+  @override
+  State<MainTabView> createState() => _MainTabViewState();
+}
+
+class _MainTabViewState extends State<MainTabView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: const Column(
+        children: [
+          Spacer(),
+          Column(
+  children: [
+    Stack(
+
+    )
+  ],
+)
+
+
+        ],
+      ),
+    );
+  }
+}
